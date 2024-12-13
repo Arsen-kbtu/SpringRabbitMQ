6e89fc1ceca1e9b36c68d58fe334dd3c6d9e8660
Arsen-kbtu <a_zhexenbiyev@kbtu.kz>
2024-12-13 18:45:57 +0500

    fix

--- spring-amqp/src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Sender.java
+++ spring-amqp/src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Sender.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.amqp.tutorials.tut1;
 
 import org.springframework.amqp.core.Queue;
@@ -20,10 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
-/**
- * @author Gary Russell
- * @author Scott Deeg
- */
+
 public class Tut1Sender {
 
 	@Autowired
